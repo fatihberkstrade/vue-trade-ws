967ca091539fd378cedb447899904616f99c874c
fatihberkstrade <112577447+fatihberkstrade@users.noreply.github.com>
2022-09-01 02:37:12 +0300

    Update Trades.vue

--- src/components/Trades.vue
+++ src/components/Trades.vue
@@ -4,23 +4,27 @@
       <input type="text" v-model="pair" />
       <button @click="update">Update</button>
     </h3>
-    <h3>
-      Buy: {{ totalBuy.toFixed(2) }} Threshold Buy:
-      {{ thresholdTotalBuy.toFixed(2) }}
-    </h3>
-    <h3>
-      Sells: {{ totalSell.toFixed(2) }} Threshold Sells:
-      {{ thresholdTotalSell.toFixed(2) }}
-    </h3>
-    <h3>
-      {{ (totalBuy / totalSell).toFixed(3) }} -
-      {{ (thresholdTotalBuy / thresholdTotalSell).toFixed(3) }}
-    </h3>
-    <h3>{{ minPrice }} - {{ maxPrice }}</h3>
-    <h3 class="main-h3">
+    <div class="header-div">
+      <h3>Buy: {{ totalBuy.toFixed(2) }}</h3>
+      <h3>Sells: {{ totalSell.toFixed(2) }}</h3>
+      <h3>
+        Threshold Buy:
+        {{ thresholdTotalBuy.toFixed(2) }}
+      </h3>
+      <h3>
+        Threshold Sells:
+        {{ thresholdTotalSell.toFixed(2) }}
+      </h3>
+      <h3>
+        {{ (totalBuy / totalSell).toFixed(3) }} -
+        {{ (thresholdTotalBuy / thresholdTotalSell).toFixed(3) }}
+      </h3>
+      <h3>{{ minPrice }} - {{ maxPrice }}</h3>
+    </div>
+    <h3 class="main-div">
       <table>
         <tr
-          v-for="trade in trades"
+          v-for="trade in displayTrades"
           :key="trade.index"
           :class="{ sell: trade.isSell, buy: !trade.isSell }"
         >
@@ -33,7 +37,7 @@
         </tr>
       </table>
       <table>
-        <tr v-for="trade in buys" :key="trade.index" class="buy">
+        <tr v-for="trade in thresholdBuys" :key="trade.index" class="buy">
           <td>
             {{ trade.amount.toFixed(2) }}
           </td>
@@ -44,7 +48,7 @@
         </tr>
       </table>
       <table>
-        <tr v-for="trade in sells" :key="trade.index" class="sell">
+        <tr v-for="trade in thresholdSells" :key="trade.index" class="sell">
           <td>
             {{ trade.amount.toFixed(2) }}
           </td>
@@ -59,22 +63,108 @@
 </template>
 
 <script>
+import _ from "lodash";
 export default {
   name: "Trades",
   data() {
     return {
       pair: "btc",
-      sells: [],
-      buys: [],
-      thresholdTotalSell: 0,
-      thresholdTotalBuy: 0,
-      totalSell: 0,
-      totalBuy: 0,
       trades: [],
-      minPrice: 0,
-      maxPrice: 0,
       ws: undefined,
     };
+  },
+  computed: {
+    displayTrades() {
+      return this.trades.slice(0, 20);
+    },
+    totalBuy() {
+      let total = 0;
+      this.buys.forEach((element) => {
+        total += element.amount;
+      });
+      return total;
+    },
+    totalSell() {
+      let total = 0;
+      this.sells.forEach((element) => {
+        total += element.amount;
+      });
+      return total;
+    },
+    thresholdTotalBuy() {
+      let total = 0;
+      this.trades.forEach((element) => {
+        if (!element.isSell && element.amount >= 100000) {
+          total += element.amount;
+        }
+      });
+      return total;
+    },
+    thresholdTotalSell() {
+      let total = 0;
+      this.trades.forEach((element) => {
+        if (element.isSell && element.amount >= 100000) {
+          total += element.amount;
+        }
+      });
+      return total;
+    },
+    buys() {
+      let returnArray = [];
+      this.trades.forEach((trade) => {
+        if (!trade.isSell) {
+          returnArray.push(trade);
+        }
+      });
+      return returnArray;
+    },
+    sells() {
+      let returnArray = [];
+      this.trades.forEach((trade) => {
+        if (trade.isSell) {
+          returnArray.push(trade);
+        }
+      });
+      return returnArray;
+    },
+    thresholdBuys() {
+      let returnArray = [];
+      this.buys.forEach((trade) => {
+        if (trade.amount > 100000) {
+          returnArray.push(trade);
+        }
+      });
+      return returnArray;
+    },
+    thresholdSells() {
+      let returnArray = [];
+      this.sells.forEach((trade) => {
+        if (trade.amount > 100000) {
+          returnArray.push(trade);
+        }
+      });
+      return returnArray;
+    },
+    minPrice() {
+      var item = _.minBy(this.trades, function (item) {
+        return item.price;
+      });
+      if (item) {
+        return item.price;
+      } else {
+        return 0;
+      }
+    },
+    maxPrice() {
+      var item = _.maxBy(this.trades, function (item) {
+        return item.price;
+      });
+      if (item) {
+        return item.price;
+      } else {
+        return 0;
+      }
+    },
   },
   methods: {
     processData() {
@@ -85,14 +175,6 @@ export default {
         index++;
         var data = JSON.parse(event.data);
         const price = Number(data.p);
-        if (that.minPrice === 0) {
-          that.minPrice = price;
-        }
-        if (price > that.maxPrice) {
-          that.maxPrice = price;
-        } else if (price < that.minPrice) {
-          that.minPrice = price;
-        }
         const amount = price * Number(data.q);
         var item = {
           amount: amount,
@@ -102,33 +184,10 @@ export default {
           time: data.T,
         };
         that.trades.unshift(item);
-        that.trades = that.trades.slice(0, 20);
-        if (amount > 100000) {
-          if (data.m) {
-            that.thresholdTotalSell += amount;
-            that.sells.unshift(item);
-            that.sells = that.sells.slice(0, 20);
-          } else {
-            that.thresholdTotalBuy += amount;
-            that.buys.unshift(item);
-            that.buys = that.buys.slice(0, 20);
-          }
-        } else {
-          if (data.m) {
-            that.totalSell += amount;
-          } else {
-            that.totalBuy += amount;
-          }
-        }
+        that.trades = that.trades.slice(0, 1000);
       };
     },
     update() {
-      this.sells = [];
-      this.buys = [];
-      this.thresholdTotalSell = 0;
-      this.thresholdTotalBuy = 0;
-      this.totalSell = 0;
-      this.totalBuy = 0;
       this.trades = [];
       this.ws.close();
       this.ws = new WebSocket(
@@ -157,9 +216,17 @@ h3 {
   color: red;
 }
 
-.main-h3 {
+.main-div {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   align-items: baseline;
+}
+
+.header-div {
+  padding-left: 10vw;
+  padding-right: 10vw;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
 }
 </style>
