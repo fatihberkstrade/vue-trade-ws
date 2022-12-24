34ab6f373318ae532f7687c91cedc13cc0318a6a
fatihberkstrade <112577447+fatihberkstrade@users.noreply.github.com>
2022-12-25 00:42:03 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,20 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #999;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: 9px;
+}
+@media screen and (min-width: 800px) {
+  #app {
+    font-size: 16px;
+  }
 }
 </style>
